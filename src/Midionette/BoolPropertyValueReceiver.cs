056e6c2180b67d923e0ec43950880538688c06c8
take@elgraiv <4583273+elgraiv-take@users.noreply.github.com>
2018-08-09 01:23:33 +0900

    ボタン系のCCようにbool用のレシーバーを追加

---
+++ src/Midionette/BoolPropertyValueReceiver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elgraiv.Midionette
+{
+    public class BoolPropertyValueReceiver : PropertyValueReceiverBase
+    {
+        public BoolPropertyValueReceiver(string name, object targetObject, string propertyName) 
+            : base(name, targetObject, propertyName)
+        {
+        }
+
+        protected override object ConvertValue(byte value)
+        {
+            return value > 63;
+        }
+    }
+}
